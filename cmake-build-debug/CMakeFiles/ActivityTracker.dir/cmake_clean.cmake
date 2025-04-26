3832f360dd18970dcdcebdb4c931dd3e2f13c7f0
freddyGut <alexandro.ghergut@edu.unifi.it>
2025-04-26 16:00:09 +0200

    Initial commit - setting up CMake file

---
+++ cmake-build-debug/CMakeFiles/ActivityTracker.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "ActivityTracker.exe"
+  "ActivityTracker.exe.manifest"
+  "ActivityTracker.pdb"
+  "CMakeFiles/ActivityTracker.dir/main.cpp.obj"
+  "CMakeFiles/ActivityTracker.dir/main.cpp.obj.d"
+  "libActivityTracker.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ActivityTracker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
