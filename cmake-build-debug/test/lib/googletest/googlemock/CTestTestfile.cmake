2da7dec3b9c3568d3c90c94463b05576fb044217
freddyGut <alexandro.ghergut@edu.unifi.it>
2025-05-06 22:19:05 +0200

    Removed .idea and build directories from Git tracking

--- cmake-build-debug/test/lib/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Jonathan/CLionProjects/ActivityTracker/test/lib/googletest/googlemock
-# Build directory: C:/Users/Jonathan/CLionProjects/ActivityTracker/cmake-build-debug/test/lib/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
